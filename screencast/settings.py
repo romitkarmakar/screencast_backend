29cf04a3e3b7c746003b3c6cd29eb3544ad5fa32
Romit Karmakar <romitkarmakar@gmail.com>
2019-08-12 14:03:26 +0530

    Removed keys

--- screencast/settings.py
+++ screencast/settings.py
@@ -148,20 +148,3 @@ CORS_ORIGIN_REGEX_WHITELIST = (
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
-
-# AWS_ACCESS_KEY_ID = 'AKIAXIBYJKTXQYXVYROA'
-# AWS_SECRET_ACCESS_KEY = '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'
-# AWS_STORAGE_BUCKET_NAME = 'screencast-assets'
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-# AWS_LOCATION = 'static'
-
-# # STATICFILES_DIRS = [
-# #     os.path.join(BASE_DIR, '/static'),
-# # ]
-# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# DEFAULT_FILE_STORAGE = 'screencast.storage_backends.MediaStorage'
