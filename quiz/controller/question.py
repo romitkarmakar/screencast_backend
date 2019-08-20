e207788842f92604957d5bfcdf6f275f743d5c94
Romit Karmakar <romitkarmakar@gmail.com>
2019-08-20 23:10:48 +0530

    Added error support

--- quiz/controller/question.py
+++ quiz/controller/question.py
@@ -16,22 +16,38 @@ def currentLevel():
             print(level.level_number)
             return level.level_number
 
+
+def nextLevel():
+    now = timezone.now()
+    levels = Level.objects.all()
+    for level in levels:
+        if now >= level.end_time:
+            continue
+        if now <= level.start_time:
+            value = str(level.start_time - now)
+            valueArr = value.split(".")
+            return [valueArr[0], level.level_number]
+
+
 def getLevel(request):
-    return HttpResponse(currentLevel())
+    if currentLevel():
+        return JsonResponse({"status": 200, "level": currentLevel()})
+    else:
+        return JsonResponse({"status": 400, "level": nextLevel()[1], "message": nextLevel()[0]})
 
 
 def getQuestion(request):
     currLevel = currentLevel()
     question = {}
-    email = request.GET.get('email')
+    email = request.GET.get("email")
     if verifyUser(email):
         user = Player.objects.get(email=email)
         score_array = user.score.split(",")
-        score = int(score_array[currLevel-1])
-        q_num = int((score/10))
+        score = int(score_array[currLevel - 1])
+        q_num = int((score / 10))
 
         level = Level.objects.get(level_number=currLevel)
-        questions = Question.objects.filter(level=level).order_by('pk')
+        questions = Question.objects.filter(level=level).order_by("pk")
         # questions.reverse()
 
         for index, q in enumerate(questions):
@@ -39,63 +55,65 @@ def getQuestion(request):
                 question = questions[index]
                 img_url = request.build_absolute_uri(question.image.url)
                 audio_url = request.build_absolute_uri(question.audio.url)
+                return JsonResponse(
+                    {
+                        "status": 200,
+                        "question": question.question_text,
+                        "hint": question.answer_text,
+                        "score": score,
+                        "image": img_url,
+                        "audio": audio_url,
+                    }
+                )
+            else:
                 return JsonResponse({
-                    'question': question.question_text,
-                    'hint': question.answer_text,
-                    'score': score,
-                    'image': img_url,
-                    'audio': audio_url,
+                    "status": 500,
+                    "message": "No questions"
                 })
 
     else:
-        return JsonResponse({
-            'status': 404,
-            'message': "User Not Registered"
-        })
+        return JsonResponse({"status": 404, "message": "User Not Registered"})
 
 
 def checkAnswer(request):
     currLevel = currentLevel()
-    email = request.GET.get('email')
+    email = request.GET.get("email")
     user = Player.objects.get(email=email)
     score_array = user.score.split(",")
     print(score_array)
-    score = int(score_array[currentLevel()-1])
-    q_num = int((score/10))
+    score = int(score_array[currentLevel() - 1])
+    q_num = int((score / 10))
     level = Level.objects.get(level_number=currLevel)
-    questions = Question.objects.filter(level=level).order_by('pk')
+    questions = Question.objects.filter(level=level).order_by("pk")
     for index, q in enumerate(questions):
         if q_num == index:
             question = questions[index]
-            answer = request.GET.get('answer')
+            answer = request.GET.get("answer")
             answer = answer.lower()
             answer = answer.strip()
             if question.checkAnswer(answer):
-                score_array[currLevel-1] = str(int(score_array[currLevel-1]) + 10)
+                score_array[currLevel - 1] = str(int(score_array[currLevel - 1]) + 10)
                 user.submit_time = timezone.now()
                 user.score = ",".join(score_array)
-                user.total_score += 10
                 user.save()
-                return JsonResponse({
-                    'isTrue': 1
-                })
+                return JsonResponse({"isTrue": 1})
             else:
-                return JsonResponse({
-                    'isTrue': 0
-                })
+                return JsonResponse({"isTrue": 0})
 
 
 def leaderboard(request):
-    p = Player.objects.order_by('-total_score', 'submit_time')
+    p = Player.objects.order_by("-total_score", "submit_time")
     current_rank = 1
     players_array = []
     for player in p:
         player.rank = current_rank
-        players_array.append({
-            'name': player.name,
-            'rank': player.rank,
-            'score': player.total_score,
-            'image': player.image,
-        })
+        players_array.append(
+            {
+                "name": player.name,
+                "rank": player.rank,
+                "score": player.total_score,
+                "image": player.image,
+            }
+        )
         current_rank += 1
     return JsonResponse(players_array, safe=False)
