266f301bb4482faee81426d54676cabaf24d098c
Romit Karmakar <romitkarmakar@gmail.com>
2019-08-24 11:26:08 +0530

    Expaned question length

--- quiz/models/Question.py
+++ quiz/models/Question.py
@@ -2,7 +2,7 @@ from django.db import models
 from quiz.models.Level import Level
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=750)
     answer_text =  models.CharField(max_length=200)
     hint = models.CharField(max_length=200,blank=True)
     image = models.ImageField(upload_to='images',default="Not Available", blank=True)
